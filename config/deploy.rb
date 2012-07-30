e8ea9eb3f7b2585838bd5aaad73171a13d4bfa4b
Ryan Ming <ryan.ming@gmail.com>
2012-07-30 19:02:20 -0400

    fixed incorrect repo reference

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:rming/#{application}.git"
+set :repository, "git@github.com:rynmng/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
